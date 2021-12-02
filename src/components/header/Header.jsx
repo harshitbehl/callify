e512155323a89b8a4b7fd3ba02733681634f8b72
Harshit Behl <harshit.behl.96@gmail.com>
2021-12-02 02:59:02 -0500

    UI Enhancements, Transitions & Animations

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BsGithub } from "react-icons/bs";
 
 import "./Header.scss";
 
@@ -6,6 +7,9 @@ function Header({ logo }) {
   return (
     <header className="header">
       <h1 className="header__logo">{logo}</h1>
+      <a href="https://github.com/harshitbehl/callify">
+        <BsGithub className="header__icon" />
+      </a>
     </header>
   );
 }
